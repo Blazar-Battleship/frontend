b1f580918f321362c2525a19a6b8b58be0cba434
Ali Sher Khan <asherkhan982@gmail.com>
2023-04-23 23:43:59 +0200

    added sounds

--- src/app/components/grid/grid.component.ts
+++ src/app/components/grid/grid.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { GameService } from 'src/app/services/game.service';
 import { GridCell } from 'src/app/types';
+import { Howl } from 'howler';
 
 @Component({
   selector: 'app-grid',
@@ -9,13 +10,20 @@ import { GridCell } from 'src/app/types';
 })
 export class GridComponent implements OnInit {
   @Input() enemy = false;
-  @Output() finishTurn = new EventEmitter()
+  @Output() finishTurn = new EventEmitter();
 
   gridSize = 10;
   grid: GridCell[] = [];
   selectedCell: undefined | GridCell = undefined;
-
   isGridDisabled = false;
+
+  // sound
+  hitSound = new Howl({
+    src: '../../assets/ship-hit.mp3',
+  });
+  splashSound = new Howl({
+    src: '../../assets/splash.mp3',
+  });
 
   constructor(private game: GameService) {}
 
@@ -27,7 +35,6 @@ export class GridComponent implements OnInit {
       }
     }
     this.grid = arr;
-
   }
 
   getRandomBoolean() {
@@ -36,16 +43,23 @@ export class GridComponent implements OnInit {
 
   getShotResult() {
     let result = this.game.getAttackResult(this.selectedCell!);
-     result.subscribe(res => this.selectedCell!.status = res.isShipHit );
-    this.isGridDisabled = true
+    result.subscribe((res) => {
+      this.selectedCell!.status = res.isShipHit;
+      if ( res.isShipHit === true) {
+        this.hitSound.play();
+      } else {
+
+        this.splashSound.play();
+      }
+    });
+    this.isGridDisabled = true;
     console.log(this.selectedCell?.status);
   }
 
-
-  handleTurnFinished(){
-    this.finishTurn.emit()
-    this.isGridDisabled=false
-    this.selectedCell = undefined
+  handleTurnFinished() {
+    this.finishTurn.emit();
+    this.isGridDisabled = false;
+    this.selectedCell = undefined;
   }
   test() {}
 }
