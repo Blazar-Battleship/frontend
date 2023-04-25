cc476e3c22a4d647f9272c77abf6b35712c80711
ilagjo <ilaria.clg@outlook.it>
2023-04-25 12:24:38 +0200

    modifiche controlli

--- src/app/components/registration-form/registration-form.component.ts
+++ src/app/components/registration-form/registration-form.component.ts
@@ -37,38 +37,44 @@ export class RegistrationFormComponent {
 
   checkInputValidity() {
     const playerToAdd = this.registrationForm.get('playerToAdd');
-    if (playerToAdd && playerToAdd.dirty && !playerToAdd.errors) {
+    if (playerToAdd && playerToAdd.dirty) {
       const value = playerToAdd.value.trim();
-      if (value.length > 20) {
-        this.showError = true;
-        this.disableAddButton = true;
-        playerToAdd.setErrors({ maxlength: true });
-      } else if (
-        this.list.find((player) => player.toLowerCase() === value.toLowerCase())
-      ) {
-        this.showError = true;
-        this.disableAddButton = true;
-        playerToAdd.setErrors({ exists: true });
+      if (value.length > 0 && value.length <= 20) {
+        if (this.list.find((player) => player.toLowerCase() === value.toLowerCase())) {
+          this.disableAddButton = true;
+          playerToAdd.setErrors({ exists: true });
+        } else {
+          this.disableAddButton = false;
+          playerToAdd.setErrors(null);
+        }
+       // this.showError = false;
       } else {
-        this.showError = false;
+        if (value.length === 0) {
+          playerToAdd.setErrors({ pattern: true });
+        } else if (value.length > 20) {
+          playerToAdd.setErrors({ maxlength: true });
+        }
         this.disableAddButton = false;
-        playerToAdd.setErrors(null);
+        this.showError = true;
       }
     }
   }
+
+
+
 
   isValidPlayer() {
     const playerToAdd = this.registrationForm.get('playerToAdd')?.value;
     return (
       !this.disableAddButton &&
       playerToAdd &&
-      playerToAdd.length > 0 &&
+      playerToAdd.trim().length > 0 &&
       playerToAdd.length <= 20 &&
-      !this.registrationForm.get('playerToAdd')?.hasError('pattern') &&
       !this.registrationForm.get('playerToAdd')?.hasError('maxlength') &&
       !this.registrationForm.get('playerToAdd')?.hasError('exists')
     );
   }
+
 
   addToList() {
     const playerToAdd = this.registrationForm.get('playerToAdd');
@@ -100,14 +106,17 @@ export class RegistrationFormComponent {
     playerToAdd?.markAsUntouched();
     playerToAdd?.markAsPristine();
     playerToAdd?.updateValueAndValidity();
+    this.showError = false;
   }
 
   removeFromList(index: number) {
     this.list.splice(index, 1);
     this.registrationForm.get('playerToAdd')?.markAsUntouched();
-    this.showError = false; // aggiungi questa riga per nascondere l'errore dopo aver svuotato l'input
+    this.showError = false;
   this.disableAddButton = false;
-  }  updateInputHint() {
+  }
+
+  updateInputHint() {
     const inputLength = this.getInputLength();
     this.showHint = inputLength > 0 && inputLength < 20;
   }
