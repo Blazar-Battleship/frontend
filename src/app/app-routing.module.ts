47e0dca1f785ea1de165d9df3aba750fa8044547
Ali Sher Khan <asherkhan982@gmail.com>
2023-04-25 13:12:57 +0200

    fixed routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PlayScreenComponent } from './components/play-screen/play-screen.component';
 import { PlaceShipsScreenComponent } from './components/place-ships-screen/place-ships-screen.component';
+import { RegistrationFormComponent } from './components/registration-form/registration-form.component';
 
 const routes: Routes = [
   {path:"play" , component:PlayScreenComponent},
   {path:"place-ships" , component:PlaceShipsScreenComponent},
+  {path:"players-form" , component:RegistrationFormComponent},
+
 ];
 
 @NgModule({
